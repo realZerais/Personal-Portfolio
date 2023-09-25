17b7e6fa78a921469d25690f1384a84fcb186842
zerais <davesiarez18@gmail.com>
2023-09-25 20:23:27 +0800

    layout done

---
+++ script.js
@@ -0,0 +1,9 @@
+// to do:
+// - interactive background
+// - I am a ...
+// - sticky navbar then change color on scroll
+// - navbar scroll on click
+// - scroll animation
+// - portfolio cards then expand on lick
+// - skills progess bar 
+// - about

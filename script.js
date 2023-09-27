e78aedd77bf8d3de11db82185e2037618c030cd0
zerais <davesiarez18@gmail.com>
2023-09-27 17:49:29 +0800

    navbar refactored and scroll on click links

--- script.js
+++ script.js
@@ -2,13 +2,35 @@
 // - interactive background
 // - I am a ...
 // - sticky navbar then change color on scroll
-// - navbar scroll on click
+// - navbar scroll on click(done)*
 // - scroll animation
 // - portfolio cards then expand on lick
 // - skills progess bar 
 // - about
 // - scrollbar
 
+// window.onscroll = () => {
+//     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//         document.getElementById("navbar").style.padding = "30px 10px";
+//         document.getElementById("logo").style.fontSize = "25px";
+//       } else {
+//         document.getElementById("navbar").style.padding = "80px 10px";
+//         document.getElementById("logo").style.fontSize = "35px";
+//       }
+// };
+
+window.onscroll = function() {scrollFunction()};
+
+var navBar = document.querySelector(".navbar-container");
 
 
-console.log("tesat")
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    navBar.style.backgroundColor = "#2d2c2c";
+    navBar.style.opacity = "0.9";
+  } else {
+    navBar.style.backgroundColor = "black";
+    navBar.style.opacity = "1";
+  }
+}
+
